42367bd51305805508b0071f532adde4262cf861
orkunkl <kulceorkun@gmail.com>
2020-09-28 18:59:03 +0300

    Prepare index sample

--- contracts/cw20-atomic-swap/src/state.rs
+++ contracts/cw20-atomic-swap/src/state.rs
@@ -2,9 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use crate::balance::Balance;
-use cosmwasm_std::{
-    Binary, BlockInfo, CanonicalAddr, Order, ReadonlyStorage, StdError, StdResult, Storage,
-};
+use cosmwasm_std::{Binary, BlockInfo, CanonicalAddr, Order, ReadonlyStorage, StdError, StdResult, Storage };
 use cosmwasm_storage::{bucket, bucket_read, prefixed_read, Bucket, ReadonlyBucket};
 use cw20::Expiration;
 
@@ -26,6 +24,20 @@ impl AtomicSwap {
 }
 
 pub const PREFIX_SWAP: &[u8] = b"atomic_swap";
+pub const RECIPIENT_INDEX: &[u8] = b"asri";
+const MARKER_VALUE: u64 = 0u64;
+
+/// Returns a bucket with all swaps (query by id)
+pub fn create_atomic_swap<S: Storage>(storage: &mut S, key: &[u8], a: &AtomicSwap) -> StdResult<()> {
+    atomic_swaps(storage).save(&key, a)?;
+    atomic_swaps_recipient_index(storage, a.recipient.as_slice())
+        .save(key, &MARKER_VALUE)
+}
+
+// (Secondary index, primary id) -> u64
+pub fn atomic_swaps_recipient_index<'a, S: Storage>(storage: &'a mut S, rec: &[u8]) -> Bucket<'a, S, u64> {
+    Bucket::multilevel(&[RECIPIENT_INDEX, rec], storage)
+}
 
 /// Returns a bucket with all swaps (query by id)
 pub fn atomic_swaps<S: Storage>(storage: &mut S) -> Bucket<S, AtomicSwap> {
@@ -93,5 +105,48 @@ mod tests {
             vec!["assign".to_string(), "lazy".to_string(), "zen".to_string()],
             ids
         )
+    }
+
+    #[test]
+    fn test_atomic_swap_recipient_index() {
+        let mut storage = MockStorage::new();
+        let recipient1 = b"0";
+        let key11= 00u8;
+        let recipient2 = b"1";
+        let key21 = 01u8;
+        let key22 = 02u8;
+
+        let aswap1 = AtomicSwap {
+            recipient: CanonicalAddr(Binary(recipient1.to_vec())),
+            ..AtomicSwap::default()
+        };
+        create_atomic_swap(&mut storage, &vec![key11], &aswap1).unwrap();
+
+        let aswap2 = AtomicSwap {
+            recipient: CanonicalAddr(Binary(recipient2.to_vec())),
+            ..AtomicSwap::default()
+        };
+        create_atomic_swap(&mut storage, &vec![key21], &aswap2).unwrap();
+        create_atomic_swap(&mut storage, &vec![key22], &aswap2).unwrap();
+
+        // first recipient
+        let res: StdResult<Vec<Vec<u8>>> = atomic_swaps_recipient_index(&mut storage, recipient1)
+            .range(None, None, Order::Ascending)
+            .map(|item| {
+                let (k, _) = item?;
+                Ok(k)
+            }).collect();
+
+        assert_eq!(vec![key11], res.unwrap().concat());
+
+        // second recipient
+        let res: StdResult<Vec<Vec<u8>>> = atomic_swaps_recipient_index(&mut storage, recipient2)
+            .range(None, None, Order::Ascending)
+            .map(|item| {
+                let (k, _) = item?;
+                return Ok(k)
+            }).collect();
+
+        assert_eq!(vec![key21, key22], res.unwrap().concat());
     }
 }
